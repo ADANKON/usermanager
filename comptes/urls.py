f22645c76eaf1e26d8d72b2b5f916e97cb96d6b0
ADANKON <wiltordadankon720@gmail.com>
2025-05-08 18:52:43 +0100

    les migrations

--- comptes/urls.py
+++ comptes/urls.py
@@ -1,5 +1,5 @@
 from .views import connexion, inscription, accueil, deconnexion
-
+from django.urls import path
 urlpatterns = [
     path('login/', connexion, name='login'),
     path('register/', inscription, name='register'),
